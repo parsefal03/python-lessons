e3f701dc8a02c59c6bce865006e0722ad2c50ad4
01|02|2003 <107813832+parsefal03@users.noreply.github.com>
2022-06-21 03:17:08 +0500

    Yangi fayllar yuklandi!

---
+++ python-amaliy/hello_world.py
@@ -0,0 +1,17 @@
+# Kirish.
+# Sana 21/06/22
+# Muallif: Jasurbek
+
+# quydagi kod hello world bilan salom dunyoni kocolga chiqaradi.
+print("Hello world!")
+
+print("Salom dunyo!")
+
+
+# VAZIFA
+# print yordamida quydagilarni bajaring:
+# Assalomu alaykum!
+# Hayrli tong!
+# 2+4*2
+# 19/5
+# 2**4
---
+++ python-amaliy/print-funksiyasi.py
@@ -0,0 +1,56 @@
+# print() funksiyasi
+# Sana 21/06/22
+# Muallif: Jasurbek
+
+#qo'sh tirnoq va bir tirnoqlarga etibor bering:
+
+print('Men "lenovo" markasidagi noutbuk sotib oldim')
+print("Men 'lenovo' markasidagi noutbuk sotib oldim")
+print("Men \"lenovo\" markasidagi noutbuk sotib oldim")
+
+# matinni bir necha qator qilib chiqarish:
+# 1-usul
+print("""Agar bilmaydigan bo'lsanggiz,
+ahli ilmlardan so'ranglar.""")
+# 2-usul: \n 
+print("Agar bilmaydigan bo'lsanggiz,\n ahli ilmlardan so'ranglar.")
+ 
+# O' bilan g' harflarnini bir tirnoq ichida yozish : \ usuli
+print('Agar bilmaydigan bo\'lsanggiz, ahli ilmlardan so\'ranglar. ')
+
+# Arifmetik amallar:
+print(2+2*2)
+print(21/5)
+print(25/5)
+# Sonni butun holida olish usuli:
+print(25/4)
+print(25//4) # natijani ko'ring
+# ___________________________
+# sonlarni darajaga ko'tarish
+print(2//4)
+print(5//2)
+
+#  matn va ifoda birgalikda:
+# 1-usul
+print("Beshning kvadrati", 5**2, "ga teng")
+# 2-usul
+print("3 * 3 =",3*3)
+
+
+# VAZIFA
+# 1
+# quydagi matnni koncolga chiqaring:
+#  "Molibu 2", "Jentra", "Spark" ko'rganlar qilar havas.
+# 2
+# 5 ning 4-darajasini toping:
+# 3
+# 25 ni 7 ga bo'lganda qancha qoldiq qoladi?
+# 4
+# tomonlari 25 bo'lgan kvadratning yuzi va perimetirini toping.
+# 5
+# diametiri 16 ga teng bo'lgan doiraning yuzini toping(P=3.14 deb oling.)
+# 6
+# katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping.
+#(pifagor teoremasidan foydalaning)
+
+# JAVOBLARNI SAVOL VA YECHIM KO'RINISHIDA CHIQARING: 5 ning 4-darajasi 625 ga teng.  
